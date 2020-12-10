795cab4afa5dd87b151cb7975d072362bae0e96f
Rob B <robbarlw@gmail.com>
2020-12-09 23:34:07 -0500

    spent a couple hours writing a multiprocessing version of the policy gradient reinforcement function instead of doing the blog post

--- src/policy_gradient/policy_gradient.py
+++ src/policy_gradient/policy_gradient.py
@@ -9,8 +9,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.autograd import Variable
-from torch.distributions import Categorical
+import pickle
+import multiprocessing
+from multiprocessing import Pool
+from itertools import starmap
+
+
 
 
 
@@ -41,7 +45,7 @@ def discount_rewards(rewards, gamma=0.99):
 
 
 def reinforce(env, policy_estimator, num_episodes=2000,
-              batch_size=10, gamma=0.99):
+              batch_size=5, gamma=0.999):
     # Set up lists to hold results
     total_rewards = []
     batch_rewards = []
@@ -50,97 +54,179 @@ def reinforce(env, policy_estimator, num_episodes=2000,
     batch_counter = 1
     
     # Define optimizer
-    optimizer = optim.Adam(policy_estimator.network.parameters(), lr=0.01)
+    optimizer = optim.Adam(policy_estimator.network.parameters(), lr=0.005)
     
     action_space = np.arange(env.action_space.n)
     ep = 0
 
+    
+
     while ep < num_episodes:
-        s_0 = env.reset()
-        states = []
-        rewards = []
-        actions = []
-        done = False
-        while done == False:
-            # Get actions and sample an action
-            action_probs = policy_estimator.predict(s_0).detach().numpy()
-            action = np.random.choice(action_space, p=action_probs)
+        
+        pool_mapping = [(env, policy_estimator, action_space) for i in range(batch_size)]
+
+        batch = starmap(run_episode, pool_mapping)
+
+        for states, actions, rewards in batch:
             
-            # take a step in the environment
-            s_1, r, done, _ = env.step(action)
+            batch_rewards.extend(discount_rewards(rewards, gamma))
+
+            batch_states.extend(states)
             
-            #append items to our lists
-            states.append(s_0)
-            rewards.append(r)
-            actions.append(action)
-            s_0 = s_1
+            batch_actions.extend(actions)
+                           
+            total_rewards.append(sum(rewards))
+        
+                
+        #print("\r \n ", "optimizing       ", end="\033[F")
+
+        #set gradients to 0
+        optimizer.zero_grad()
+
+        #create tensors of our states and rewards
+        state_tensor = torch.FloatTensor(batch_states)
+
+        reward_tensor = torch.FloatTensor(batch_rewards)
+        
+        action_tensor = torch.LongTensor(batch_actions)
+        
+        # Calculate the log probability of all actions at all states
+        logprob = torch.log(policy_estimator.predict(state_tensor))
+
+        action_tensor = torch.reshape(action_tensor, (len(action_tensor), 1))
+
+        selected_logprobs = reward_tensor * torch.gather(logprob, 1, action_tensor).squeeze()
+
+        loss = -selected_logprobs.mean()
+        
+        # Calculate gradients
+        loss.backward()
+        # Apply gradients
+        optimizer.step()
+        
+        batch_rewards = []
+        batch_actions = []
+        batch_states = []
             
-            # If done, batch data
-            if done:
-                batch_rewards.extend(discount_rewards(rewards, gamma))
-                
-                batch_states.extend(states)
-                
-                batch_actions.extend(actions)
-                
-                batch_counter += 1
-               
-                total_rewards.append(sum(rewards))
-                
-                # If batch is complete, update network
-                if batch_counter == batch_size:
-                    
-                    #set gradients to 0
-                    optimizer.zero_grad()
-
-                    #create tensors of our states and rewards
-                    state_tensor = torch.FloatTensor(batch_states)
-                    reward_tensor = torch.FloatTensor(batch_rewards)
-                    action_tensor = torch.LongTensor(batch_actions)
-                    
-                    # Calculate the log probability of all actions at all states
-                    logprob = torch.log(policy_estimator.predict(state_tensor))
-
-                    action_tensor = torch.reshape(action_tensor, (len(action_tensor), 1))
-
-                    selected_logprobs = reward_tensor * torch.gather(logprob, 1, action_tensor).squeeze()
-
-                    loss = -selected_logprobs.mean()
-                    
-                    # Calculate gradients
-                    loss.backward()
-                    # Apply gradients
-                    optimizer.step()
-                    
-                    batch_rewards = []
-                    batch_actions = []
-                    batch_states = []
-                    batch_counter = 1
-                    
-                avg_rewards = np.mean(total_rewards[-100:])
-                # Print running average
-                print("\r", ep, avg_rewards, end="")
-                ep += 1
+        avg_rewards = np.mean(total_rewards[-100:])
+        # Print running average
+        print("\r \n \n", ep, avg_rewards, end="\033[F \033[F")
+        ep += batch_size
                 
     return total_rewards
 
 
 
-env = gym.make('LunarLander-v2')
-policy_est = policy_estimator_network(env)
-rewards = reinforce(env, policy_est, num_episodes = 1000)
 
 
-s_0 = env.reset()
-action_space = np.arange(env.action_space.n)
-done = False
 
-for _ in range(1000):
-    env.render()
-    if not done:
-        action_probs = policy_est.predict(s_0).detach().numpy()
+
+def run_episode(env, policy_estimator, action_space):
+    print(policy_estimator)
+    s_0 = env.reset()
+    states = []
+    rewards = []
+    actions = []
+    done = False
+    while done == False:
+        # Get actions and sample an action
+        action_probs = policy_estimator.predict(s_0).detach().numpy()
         action = np.random.choice(action_space, p=action_probs)
+        
+        #print("\r \n ",len(states), "        step",  end="\033[F")
+
+        # take a step in the environment
         s_1, r, done, _ = env.step(action)
+
+        #print("\r \n ", len(states), "        post step", end="\033[F")
+        #append items to our lists
+        states.append(s_0)
+        rewards.append(r)
+        actions.append(action)
         s_0 = s_1
-    else: break
-env.close()
+
+    return (states, actions, rewards)
+
+
+
+
+def reinforce_multiprocess(envs, policy_estimator, num_episodes=2000, gamma=0.999):
+    # Set up lists to hold results
+    total_rewards = []
+    batch_rewards = []
+    batch_actions = []
+    batch_states = []
+
+    action_space = np.arange(envs[0].action_space.n)
+
+    # Define optimizer
+    optimizer = optim.Adam(policy_estimator.network.parameters(), lr=0.005)
+
+    action_space = np.arange(envs[0].action_space.n)
+    ep = 0
+
+    pool_mapping = [(env, policy_estimator, action_space) for env in envs]
+
+    while ep < num_episodes:
+        
+        
+        with Pool(5) as p:
+            batch = starmap(run_episode, pool_mapping) #p.starmap breaks the model????????
+
+            for states, actions, rewards in batch:
+                
+                batch_rewards.extend(discount_rewards(rewards, gamma))
+
+                batch_states.extend(states)
+                
+                batch_actions.extend(actions)
+                               
+                total_rewards.append(sum(rewards))
+
+         #set gradients to 0
+        optimizer.zero_grad()
+
+        #create tensors of our states and rewards
+        state_tensor = torch.FloatTensor(batch_states)
+
+        reward_tensor = torch.FloatTensor(batch_rewards)
+        
+        action_tensor = torch.LongTensor(batch_actions)
+        
+        # Calculate the log probability of all actions at all states
+        logprob = torch.log(policy_estimator.predict(state_tensor))
+
+        action_tensor = torch.reshape(action_tensor, (len(action_tensor), 1))
+
+        selected_logprobs = reward_tensor * torch.gather(logprob, 1, action_tensor).squeeze()
+
+        loss = -selected_logprobs.mean()
+        
+        # Calculate gradients
+        loss.backward()
+        # Apply gradients
+        optimizer.step()
+        
+        batch_rewards = []
+        batch_actions = []
+        batch_states = []
+            
+        avg_rewards = np.mean(total_rewards[-100:])
+        # Print running average
+        print("\r \n \n", ep, avg_rewards, end="\033[F \033[F")
+        ep += len(envs)
+                
+    return total_rewards
+
+
+if __name__ == "__main__":
+    print()
+    gym.envs.register(
+         id='Cappedlunar-v0',
+         entry_point='gym.envs.box2d:LunarLander',
+         max_episode_steps=400,
+    )
+
+    envs = [gym.make('Cappedlunar-v0')] * 10
+    policy_est = policy_estimator_network(envs[0])
+    #print(reinforce_multiprocess(envs, policy_est, num_episodes = 1000))
